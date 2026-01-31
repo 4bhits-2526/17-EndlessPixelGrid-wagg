ce01337cac7de38a72ee5d57f9344165b68606ba
gwagner <gabriel.wagner08@htl-salzburg.ac.at>
2026-01-31 19:53:19 +0100

    Add data model for grid and input line

---
+++ Assets/Datamodel.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Datamodel : MonoBehaviour
+{
+    public bool[,] Grid { get; private set; }
+    public bool[] InputLine { get; private set; }
+
+    public const int Rows = 10;
+    public const int Columns = 7;
+
+    public void GridModel()
+    {
+        Grid = new bool[Rows, Columns];
+        InputLine = new bool[Columns];
+
+        // Initialzustand
+        ClearGrid();
+        ClearInputLine();
+    }
+
+    public void ClearGrid()
+    {
+        for (int r = 0; r < Rows; r++)
+            for (int c = 0; c < Columns; c++)
+                Grid[r, c] = false;
+    }
+
+    public void ClearInputLine()
+    {
+        for (int i = 0; i < Columns; i++)
+            InputLine[i] = false;
+    }
+}
